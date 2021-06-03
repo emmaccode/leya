34a4405a45620d8b5d87fe79021eb49890bd7b65
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2021-06-03 10:33:45 -0400

    added repl functionality

--- core/leya.asm
+++ core/leya.asm
@@ -33,10 +33,28 @@ section .data
   head_prnt5: db "( \/ ) /__\  Programmed By           |", 10
   head_prnt6: db " \  / /(__)\        Emmett Boudreau  |", 10
   head_prnt7: db " (__)(__)(__)========================|", 10
+  exit_check: db 0
+  ; Leya prompt:
   repl_prompt: db "Leya> "
+  ; Key-words
+  fn_df: db "function"
+  var_df: db "var"
+  type_df: db "type"
+  stderr: db "raise"
+  sys: db "syscall"
+  exit: db "exit"
+  end_st: db "end"
+  ; Command-line arguments
+  argcstr: db `argc = %d\n\0`
+  argvstr: db `argv[%u] = %s\n\0`
+  clis: db 0
+section .bss
+; Input
+  lya resb 64
+; For commands/arguments
+  command resb 32
 section .text
   global _start
-
 
 ;  ___  ____   __    ____  ____
 ; / __)(_  _) /__\  (  _ \(_  _)
@@ -58,11 +76,6 @@ section .text
 ; ===============|
 _start:
   jmp _start_repl
-  mov rax, 60
-  mov rdi, 0
-  syscall
-
-
 ;  ____  ____  ____  __
 ; (  _ \( ___)(  _ \(  )
 ;  )   / )__)  )___/ )(__
@@ -73,9 +86,9 @@ _start:
 ; from repl, will ret to _start. Start will then call syscall 60 (exit).
 ; ===============|
 _start_repl:
-  jmp _repl_printout
+  call _repl_printout
   jmp _repl
-  ret
+;  ret
 ; ===============|
 ; _REPL_PRINTOUT
 ; Prints leya header, returns to _start_repl:.
@@ -86,36 +99,43 @@ _repl_printout:
   mov rsi, head_prnt1
   mov rdx, 51
   syscall
+
   mov rax, 1
   mov rdi, 1
   mov rsi, head_prnt2
   mov rdx, 39
   syscall
+
   mov rax, 1
   mov rdi, 1
   mov rsi, head_prnt3
   mov rdx, 39
   syscall
+
   mov rax, 1
   mov rdi, 1
   mov rsi, head_prnt4
   mov rdx, 39
   syscall
+
   mov rax, 1
   mov rdi, 1
   mov rsi, head_prnt5
   mov rdx, 39
   syscall
+
   mov rax, 1
   mov rdi, 1
   mov rsi, head_prnt6
   mov rdx, 39
   syscall
+
   mov rax, 1
   mov rdi, 1
   mov rsi, head_prnt7
   mov rdx, 39
   syscall
+
   ret
   ; ===============|
   ; _REPL
@@ -124,9 +144,27 @@ _repl_printout:
   ; exit flag. If the exit flag has not been changed, then jumps to itself.
   ; ===============|
 _repl:
-  jmp _prompt
-  ret
+  ; Check for exit code set to 1:
+  mov rdi, 1
+  cmp rdi, exit_check
+  je _exit
+  ; Read
+  call _prompt
+  call _repl_input
+  ; Evaluate
+  call _interpret
+  ; Print
+  call _return
+  ; Loop
+  jmp _repl
 
+_repl_input:
+  mov rax, 0
+  mov rdi, 0
+  mov rsi, lya
+  mov rdx, 64
+  syscall
+  ret
   ; ===============|
   ; _PROMPT
   ; Prints prompt, returns to _repl
@@ -137,7 +175,13 @@ _prompt:
   mov rsi, repl_prompt
   mov rdx, 6
   syscall
+
   ret
+
+_exit:
+  mov rax, 60
+  mov rdi, 0
+  syscall
 ;  ____  _  _  ____  ____  ____  ____  ____  ____  ____  ____  ____
 ; (_  _)( \( )(_  _)( ___)(  _ \(  _ \(  _ \( ___)(_  _)( ___)(  _ \
 ;  _)(_  )  (   )(   )__)  )   / )___/ )   / )__)   )(   )__)  )   /
@@ -145,7 +189,19 @@ _prompt:
 ; ===============|
 ; _PARSE
 ; Parses array of input code, fills registry and data, then returns to
-; either _REPL or _RUN
+; _interpret
 ; ===============|
 _parse:
+
   ret
+_interpret:
+  call _parse
+  ret
+_return:
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, lya
+  mov rdx, 64
+  syscall
+  ret
+_compile:
